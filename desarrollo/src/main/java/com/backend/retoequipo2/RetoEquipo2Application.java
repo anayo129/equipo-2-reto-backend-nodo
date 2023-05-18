1e1fa4dc048625d615f77ae2592a2a0d82c75e93
anayo129 <j1c2m3r4@gmail.com>
2023-05-18 10:30:16 -0500

    estructura inicial del proyecto

---
+++ desarrollo/src/main/java/com/backend/retoequipo2/RetoEquipo2Application.java
@@ -0,0 +1,13 @@
+package com.backend.retoequipo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoEquipo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoEquipo2Application.class, args);
+	}
+
+}
